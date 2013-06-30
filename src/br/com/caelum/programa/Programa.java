71825c5ee4238a68354a42c71ba95e32ec93a851
João Paulo Faria <jpfaria@gmail.com>
2013-06-30 15:58:50 -0300

    .

---
+++ src/br/com/caelum/programa/Programa.java
@@ -0,0 +1,14 @@
+package br.com.caelum.programa;
+
+import br.com.caelum.cliente.CalculadorDeParcela;
+import br.com.caelum.cliente.CalculadorDeParcelaService;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		CalculadorDeParcela calculador = new CalculadorDeParcelaService()
+				.getCalculadorDeParcelaPort();
+		System.out.println(calculador.calculaParcela(100, 2));
+	}
+
+}
